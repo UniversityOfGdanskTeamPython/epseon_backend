3caa917cd70a35e3de22235fa228683877dc4b90
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2023-11-05 00:58:08 +0100

    Update project structure to align better with tools used

---
+++ python/epseon_backend/device/__init__.py
@@ -0,0 +1,2 @@
+"""Interfaces of compute devices."""
+from __future__ import annotations
--- python/test/test_example.py
+++ python/test/test_example.py
@@ -2,6 +2,9 @@
 
 from __future__ import annotations
 
+from epseon_backend import example
+
 
 def test_example() -> None:
     """Testing setup."""
+    example()
