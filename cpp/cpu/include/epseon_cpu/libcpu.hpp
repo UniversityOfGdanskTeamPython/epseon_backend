7e3d9ce3b16827cbfaa554e974f6ee066d2973bb
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2023-11-08 14:19:31 +0100

    Fix code quality issues

--- cpp/cpu/include/epseon_cpu/libcpu.hpp
+++ cpp/cpu/include/epseon_cpu/libcpu.hpp
@@ -23,11 +23,13 @@ static PyMethodDef LibCPUMethods[] = {
 // The arguments of this structure tell Python what to call your extension,
 // what it's methods are and where to look for its method definitions
 static struct PyModuleDef libepseon_cpu = {
-    PyModuleDef_HEAD_INIT, "_libepseon_cpu", /* name of module */
-    NULL,                                    /* module documentation, may be NULL */
+    PyModuleDef_HEAD_INIT,
+    "_libepseon_cpu", /* name of module */
+    NULL,             /* module documentation, may be NULL */
     -1, /* size of per-interpreter state of the module, or -1 if the module keeps state
            in global variables. */
-    LibCPUMethods};
+    LibCPUMethods
+};
 
 // Module initialization function
 PyMODINIT_FUNC PyInit__libepseon_cpu(void) {
