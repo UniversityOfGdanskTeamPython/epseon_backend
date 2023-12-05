a8aa622ae1fd725234618a2b196396aed3b17606
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2023-12-05 01:23:24 +0100

    WIP

--- cpp/gpu/source/epseon/gpu/libgpu.cpp
+++ cpp/gpu/source/epseon/gpu/libgpu.cpp
@@ -1,12 +1,5 @@
-
-
-#include "vulkan/vulkan.hpp"
-#include "vulkan/vulkan_raii.hpp"
-
 #define VMA_IMPLEMENTATION
-#define VMA_STATIC_VULKAN_FUNCTIONS 0
-#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
-#define VMA_VULKAN_VERSION 1001000
-#include "vk_mem_alloc.hpp"
+
+#include "epseon/vulkan_headers.hpp"
 
 #include "epseon/gpu/libgpu.hpp"
